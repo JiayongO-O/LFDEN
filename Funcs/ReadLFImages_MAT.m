2185f4c7a1ee2b6b05a755818631108b77e196cc
Jiayong <37617064+JiayongO-O@users.noreply.github.com>
2019-02-09 23:02:33 +0800

    Add files via upload

---
+++ Funcs/ReadLFImages_MAT.m
@@ -0,0 +1,11 @@
+function [fullLF] = ReadLFImages_MAT(scenePath)
+% If the light field images are in .mat format, use this function, 
+% else if the light field images are in macro-pixel format, use ReadLFImages.
+% MIND THE DIMENSION AND FORMATE OF THE INPUT DATA!!!
+
+load(scenePath); %the loaded data is named 'lf_data', it should be U*V*H*W*CH
+fullLF = permute(lf_data, [3,4,5,1,2]);
+fullLF = fullLF(:, :, :, 2:8, 2:8);
+fullLF = fullLF(:, :, :, end:-1:1, :);
+
+end
