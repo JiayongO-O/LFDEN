dea20d4927dade3423d59a2a2fa99899319a6e14
Jiayong <37617064+JiayongO-O@users.noreply.github.com>
2019-02-09 23:03:51 +0800

    Update ReadLFImages_MAT.m

--- Funcs/ReadLFImages_MAT.m
+++ Funcs/ReadLFImages_MAT.m
@@ -3,7 +3,7 @@ function [fullLF] = ReadLFImages_MAT(scenePath)
 % else if the light field images are in macro-pixel format, use ReadLFImages.
 % MIND THE DIMENSION AND FORMATE OF THE INPUT DATA!!!
 
-load(scenePath); %the loaded data has field name 'lf_data', it should be U*V*H*W*CH
+load(scenePath); %the loaded data has field name 'lf_data' for HCI dataset, it should be U*V*H*W*CH
 fullLF = permute(lf_data, [3,4,5,1,2]);
 fullLF = fullLF(:, :, :, 2:8, 2:8);
 fullLF = fullLF(:, :, :, end:-1:1, :);
